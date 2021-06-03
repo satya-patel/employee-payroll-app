4387a6424f38de7ff9dafba48a9a7dc01f03b5de
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-03 17:48:10 +0530

    uc22: remove emp from emp payroll details

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -21,20 +21,16 @@ const createInnerHtml = () => {
     <td>${employee._gender}</td>
     <td>${getDeptHtml(employee._department)}</td>
     <td>${employee._salary}</td>
-    <td>${employee._startDate}</td>
+    <td>${stringifyDate(employee._startDate)}</td>
     <td>
-        <img id="${employee._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-        <img id="${employee._id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+        <img id="${employee._name}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+        <img id="${employee._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
     </td>
  </tr>
     `;
     }
     document.querySelector("#display").innerHTML = innerHtml;
 };
-
-function remove(employeePayrollList) {
-    localStorage.removeItem(employeePayrollList);
-}
 
 const getDeptHtml = (deptList) => {
     let deptHtml = "";
@@ -43,3 +39,12 @@ const getDeptHtml = (deptList) => {
     }
     return deptHtml;
 };
+
+const remove = (node) => {
+    let employeeData = employeePayrollList.find(empData => empData._name == node.id);
+    if (!employeeData) return;
+    const index = employeePayrollList.map(empData => empData._name).indexOf(employeeData._name);
+    employeePayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+    createInnerHtml();
+}
