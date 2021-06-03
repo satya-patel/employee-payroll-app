ef685b7a2a753e675af8967264ddde9ee7bfa091
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-03 16:19:43 +0530

    uc10: datasetter method emp payroll add

--- javascript/employeepayroll.js
+++ javascript/employeepayroll.js
@@ -15,7 +15,11 @@ class EmployeePayroll {
     }
 
     set name(name) {
-        this._name = name;
+        let nameRegex = /[A-Z][a-z]{2,}/;
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Invalid name";
     }
 
     get picture() {
