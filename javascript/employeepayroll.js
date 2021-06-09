49890c553f7458a30259c23d81906c2163c30031
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-09 14:54:09 +0530

    uc25:test emp payroll html

--- javascript/employeepayroll.js
+++ javascript/employeepayroll.js
@@ -25,10 +25,16 @@ class EmployeePayroll {
         this._picture = picture;
     }
 
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
     get gender() {
         return this._gender;
     }
-
     set gender(gender) {
         this._gender = gender;
     }
@@ -36,23 +42,13 @@ class EmployeePayroll {
     get department() {
         return this._department;
     }
-
     set department(department) {
         this._department = department;
-    }
-
-    get salary() {
-        return this._salary;
-    }
-
-    set salary(salary) {
-        this._salary = salary;
     }
 
     get startDate() {
         return this._startDate;
     }
-
     set startDate(startDate) {
         if (startDate <= new Date()) {
             this._startDate = startDate + 1;
@@ -61,15 +57,83 @@ class EmployeePayroll {
         }
     }
 
-    get notes() {
-        return this._notes;
+    get note() {
+        return this._note;
     }
-
-    set notes(notes) {
-        this._notes = notes;
+    set note(note) {
+        this._note = note;
     }
 
     toString() {
-        return "id = " + this.id + ", name = " + this.name + ", gender = " + this.gender + ", profile picture = " + this.picture + ", department = " + this.department + ", salary = " + this.salary + ", startDate = " + this.startDate + ", note = " + this.notes;
+        return "Id: " + this.id + "\nName: " + this.name + "\nPicture: " + this.picture + "\nGender: " + this.gender + "\nDepartment: " + this.department + "\nSalary: " + this.salary + "\nStart date: " + this.startDate + "\nNote: " + this.note;
+    }
+}class EmployeePayroll {
+
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex = /[A-Z][a-z]{2,}/;
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Invalid Name";
+    }
+
+    get picture() {
+        return this._picture;
+    }
+    set picture(picture) {
+        this._picture = picture;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+    set department(department) {
+        this._department = department;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        if (startDate <= new Date()) {
+            this._startDate = startDate + 1;
+        } else {
+            throw "Invalid date";
+        }
+    }
+
+    get note() {
+        return this._note;
+    }
+    set note(note) {
+        this._note = note;
+    }
+
+    toString() {
+        return "Id: " + this.id + "\nName: " + this.name + "\nPicture: " + this.picture + "\nGender: " + this.gender + "\nDepartment: " + this.department + "\nSalary: " + this.salary + "\nStart date: " + this.startDate + "\nNote: " + this.note;
     }
 }
