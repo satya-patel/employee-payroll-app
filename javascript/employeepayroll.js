3f15a2a6c67f859b4ecb90b9c27ff7ed5a674dd9
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-09 15:07:11 +0530

    uc26:convert emp payroll to client architecture

--- javascript/employeepayroll.js
+++ javascript/employeepayroll.js
@@ -1,86 +1,12 @@
 class EmployeePayroll {
 
-    get id() {
-        return this._id;
-    }
-    set id(id) {
-        this._id = id;
-    }
+    id;
 
     get name() {
         return this._name;
     }
     set name(name) {
-        let nameRegex = /[A-Z][a-z]{2,}/;
-        if (nameRegex.test(name))
-            this._name = name;
-        else
-            throw "Invalid Name";
-    }
-
-    get picture() {
-        return this._picture;
-    }
-    set picture(picture) {
-        this._picture = picture;
-    }
-
-    get salary() {
-        return this._salary;
-    }
-    set salary(salary) {
-        this._salary = salary;
-    }
-
-    get gender() {
-        return this._gender;
-    }
-    set gender(gender) {
-        this._gender = gender;
-    }
-
-    get department() {
-        return this._department;
-    }
-    set department(department) {
-        this._department = department;
-    }
-
-    get startDate() {
-        return this._startDate;
-    }
-    set startDate(startDate) {
-        if (startDate <= new Date()) {
-            this._startDate = startDate + 1;
-        } else {
-            throw "Invalid date";
-        }
-    }
-
-    get note() {
-        return this._note;
-    }
-    set note(note) {
-        this._note = note;
-    }
-
-    toString() {
-        return "Id: " + this.id + "\nName: " + this.name + "\nPicture: " + this.picture + "\nGender: " + this.gender + "\nDepartment: " + this.department + "\nSalary: " + this.salary + "\nStart date: " + this.startDate + "\nNote: " + this.note;
-    }
-}class EmployeePayroll {
-
-    get id() {
-        return this._id;
-    }
-    set id(id) {
-        this._id = id;
-    }
-
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        let nameRegex = /[A-Z][a-z]{2,}/;
+        let nameRegex = /^[A-Z][a-z]{2,}/;
         if (nameRegex.test(name))
             this._name = name;
         else
